fb075fdd1c37c5f851074940e8bfc26921d10481
Emperor Wang <supreme.emperor.wang@gmail.com>
2023-10-19 01:29:59 +1000

    - Initial utilities

---
+++ neurons/miners/default/utils.py
@@ -0,0 +1,27 @@
+import bittensor as bt
+
+
+#### Wrapper for the raw images
+class Images:
+    def __init__(self, images):
+        self.images = images
+
+
+#### Colors to use in the logs
+COLORS = {
+    "r": "\033[1;31;40m",
+    "g": "\033[1;32;40m",
+    "b": "\033[1;34;40m",
+    "y": "\033[1;33;40m",
+    "m": "\033[1;35;40m",
+    "c": "\033[1;36;40m",
+    "w": "\033[1;37;40m",
+}
+
+
+#### Utility function for coloring logs
+def output_log(message: str, color_key: str = "w", type: str = "info") -> None:
+    log = bt.logging.info
+    if type == "debug":
+        log = bt.logging.debug
+    log(f"{COLORS[color_key]}{message}{COLORS['w']}")
