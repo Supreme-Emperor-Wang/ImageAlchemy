01c19099441fde289778cb7143df8fb087c516e7
Emperor Wang <supreme.emperor.wang@gmail.com>
2023-10-20 00:04:15 +1000

    - Renamed for easy use

--- neurons/miners/default/utils.py
+++ neurons/miners/default/utils.py
@@ -25,3 +25,7 @@ def output_log(message: str, color_key: str = "w", type: str = "info") -> None:
     if type == "debug":
         log = bt.logging.debug
     log(f"{COLORS[color_key]}{message}{COLORS['w']}")
+
+
+def sh(message: str):
+    return f"{message: <12}"
