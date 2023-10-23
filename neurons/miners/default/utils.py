012f47f18b4d3805f9d4f8471f9db81b13015780
Emperor Wang <supreme.emperor.wang@gmail.com>
2023-10-23 10:49:03 +1000

    - Added wandb utils class

--- neurons/miners/default/utils.py
+++ neurons/miners/default/utils.py
@@ -1,4 +1,7 @@
+from threading import Timer
+from typing import List
 import bittensor as bt
+import wandb
 
 
 #### Wrapper for the raw images
@@ -24,8 +27,65 @@ def output_log(message: str, color_key: str = "w", type: str = "info") -> None:
     log = bt.logging.info
     if type == "debug":
         log = bt.logging.debug
+
     log(f"{COLORS[color_key]}{message}{COLORS['w']}")
 
 
 def sh(message: str):
     return f"{message: <12}"
+
+
+#### Wandb functions
+class WandbTimer(Timer):
+    def run(self):
+        self.function(*self.args, **self.kwargs)
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+class WandbUtils:
+    def __init__(self, miner):
+        self.miner = miner
+        self.wandb = None
+        output_log(
+            f"Wandb starting run with project {self.miner.config.wandb.project} and entity {self.miner.config.wandb.entity}."
+        )
+        self.timer = WandbTimer(600, self._log, [self])
+        self.timer.start()
+
+    def _start_run(self):
+        if self.wandb:
+            self._stop_run()
+
+        #### Start new run
+
+        config = {}
+        config.update(self.args)
+        config["model"] = self.miner.config.model
+        self.wandb = wandb.init(
+            project=self.wandb.project, entity=self.wandb.entity, config=config
+        )
+
+        #### Take the first two random words plus the name of the wallet, hotkey name and uid
+        self.wandb.name = (
+            self.wandb.name.split("-")[:2]
+            + f"-{self.wallet.name}-{self.wallet.hotkey_str}-{self.args.uid}"
+        )
+        output_log(f"Started new run: {self.wandb.name}", "c")
+
+    def _stop_run(self):
+        self.wandb.finish()
+
+    def _log(self):
+        if not self.wandb:
+            self._start_run()
+            return
+        #### Log incentive, trust, emissions, total requests, timeouts
+        info = self.miner.get_miner_info()
+        info.update(
+            {
+                "total_requests": self.miner.stats.total_requests,
+                "timeouts": self.miner.stats.timeouts,
+            }
+        )
+        self.wandb.log(info)
