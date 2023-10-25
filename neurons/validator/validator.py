ff0f5ab67218b2e313351bf6477111edac417622
kmfoda <kmfoda@gmail.com>
2023-10-25 16:34:20 +0000

    Convert validator into neuron class

--- neurons/validator/validator.py
+++ neurons/validator/validator.py
@@ -112,181 +112,181 @@ def get_random_uids(self, k: int, exclude: List[int] = None) -> torch.LongTensor
     uids = torch.tensor(random.sample(available_uids, k))
     return uids
 
-# NEW IMAGENET
-async def forward(self, query, uid):
-    query_copy = query.copy()
-    timeout = 12
-    return self.dendrite(
-        self.metagraph.axons[uid],
-        synapse=query_copy,
-        timeout=timeout
-    )
-
-def main(config):
-    # Set up logging with the provided configuration and directory.
-    bt.logging(config=config, logging_dir=config.full_path)
-    bt.logging.info(
-        f"Running validator for subnet: {config.netuid} on network: {config.subtensor.chain_endpoint} with config:"
-    )
-    # Log the configuration for reference.
-    bt.logging.info(config)
-
-    # Step 4: Build Bittensor validator objects
-    # These are core Bittensor classes to interact with the network.
-    bt.logging.info("Setting up bittensor objects.")
-
-    # The wallet holds the cryptographic key pairs for the validator.
-    wallet = bt.wallet(config=config)
-    bt.logging.info(f"Wallet: {wallet}")
-
-    # The subtensor is our connection to the Bittensor blockchain.
-    subtensor = bt.subtensor(config=config)
-    bt.logging.info(f"Subtensor: {subtensor}")
-
-    # Dendrite is the RPC client; it lets us send messages to other nodes (axons) in the network.
-    dendrite = bt.dendrite(wallet=wallet)
-    bt.logging.info(f"Dendrite: {dendrite}")
-
-    # The metagraph holds the state of the network, letting us know about other validators and miners.
-    metagraph = subtensor.metagraph(config.netuid)
-    bt.logging.info(f"Metagraph: {metagraph}")
-
-    # Step 5: Connect the validator to the network
-    if wallet.hotkey.ss58_address not in metagraph.hotkeys:
-        bt.logging.error(
-            f"\nYour validator: {wallet} if not registered to chain connection: {subtensor} \nRun btcli register and try again."
+class neuron:
+    def __init__(self):
+        # Set up logging with the provided configuration and directory.
+        self.config = get_config()
+        bt.logging(config=self.config, logging_dir=self.config.full_path)
+        bt.logging.info(
+            f"Running validator for subnet: {self.config.netuid} on network: {self.config.subtensor.chain_endpoint} with config:"
         )
-        exit()
+        # Log the configuration for reference.
+        bt.logging.info(self.config)
 
-    # Each validator gets a unique identity (UID) in the network for differentiation.
-    my_subnet_uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
-    bt.logging.info(f"Running validator on uid: {my_subnet_uid}")
+        # Step 4: Build Bittensor validator objects
+        # These are core Bittensor classes to interact with the network.
+        bt.logging.info("Setting up bittensor objects.")
 
-    # Step 6: Set up initial scoring weights for validation
-    bt.logging.info("Building validation weights.")
-    scores = torch.ones_like(metagraph.S, dtype=torch.float32)
-    bt.logging.info(f"Weights: {scores}")
-    
-    # Step 7: Set up reward functions
-    reward_functions = [
-        ImageRewardModel(),
-        # DiversityRewardModel()
-    ]
-    
-    # Step 8: Set up masking functions
-    masking_functions = [
-        BlacklistFilter(),
-        NSFWRewardModel()
-    ]
+        # The wallet holds the cryptographic key pairs for the validator.
+        self.wallet = bt.wallet(config=self.config)
+        bt.logging.info(f"Wallet: {self.wallet}")
 
-    # Step 9: The Main Validation Loop
-    bt.logging.info("Starting validator loop.")
-    step = 0
-    while True:
-        try:
-            # k = 12
-            # uids = get_random_uids(self, k=k).to(self.device)
+        # The subtensor is our connection to the Bittensor blockchain.
+        self.subtensor = bt.subtensor(config=self.config)
+        bt.logging.info(f"Subtensor: {self.subtensor}")
 
-            # TODO(developer): Define how the validator selects a miner to query, how often, etc.
-            # Broadcast a query to all miners on the network.
-            # responses = dendrite.query(
-            #     # Send the query to all miners in the network.
-            #     metagraph.axons,
-            #     # Construct a dummy query.
-            #     template.protocol.Dummy(dummy_input=step),  # Construct a dummy query.
-            #     # All responses have the deserialize function called on them before returning.
-            #     deserialize=True,
-            # )
-            
-            # dendrite.query(metagraph.axons, template.protocol.Dummy(dummy_input=step), timeout = 100)
-            
-         
-            loop = asyncio.get_event_loop()
-            responses = loop.run_until_complete( dendrite(metagraph.axons, template.protocol.Dummy(dummy_input=step), timeout = 100))
-            loop.close()
+        # Dendrite is the RPC client; it lets us send messages to other nodes (axons) in the network.
+        self.dendrite = bt.dendrite(wallet=self.wallet)
+        bt.logging.info(f"Dendrite: {self.dendrite}")
 
-            breakpoint()
-            device = "cuda"
-            # Initialise rewards tensor
-            rewards: torch.FloatTensor = torch.ones(len(responses), dtype=torch.float32).to(
-                device
+        # The metagraph holds the state of the network, letting us know about other validators and miners.
+        self.metagraph = self.subtensor.metagraph(self.config.netuid)
+        bt.logging.info(f"Metagraph: {self.metagraph}")
+
+        # Step 5: Connect the validator to the network
+        if self.wallet.hotkey.ss58_address not in self.metagraph.hotkeys:
+            bt.logging.error(
+                f"\nYour validator: {self.wallet} if not registered to chain connection: {self.subtensor} \nRun btcli register and try again."
             )
-            for masking_fn_i in masking_functions:
-                mask_i, mask_i_normalized = masking_fn_i.apply(responses, )
-                rewards *= mask_i_normalized.to(device)
-                # TODO add wandb tracking
-                # if not self.config.neuron.disable_log_rewards:
-                #     event[masking_fn_i.name] = mask_i.tolist()
-                #     event[masking_fn_i.name + "_normalized"] = mask_i_normalized.tolist()
-                bt.logging.trace(str(masking_fn_i.name), mask_i_normalized.tolist())
-
-            breakpoint()
-            for weight_i, reward_fn_i in zip([0.95], reward_functions):
-                reward_i, reward_i_normalized = reward_fn_i.apply(responses)
-                rewards += weight_i * reward_i_normalized.to(device)
-                # TODO add wandb tracking
-                # if not self.config.neuron.disable_log_rewards:
-                #     event[reward_fn_i.name] = reward_i.tolist()
-                #     event[reward_fn_i.name + "_normalized"] = reward_i_normalized.tolist()
-                bt.logging.trace(str(reward_fn_i.name), reward_i_normalized.tolist())
-            breakpoint()
-            
-            # Log the results for monitoring purposes.
-            bt.logging.info(f"Received dummy responses: {responses}")
-
-            # TODO(developer): Define how the validator scores responses.
-            # Adjust the scores based on responses from miners.
-            for i, resp_i in enumerate(responses):
-                # Check if the miner has provided the correct response by doubling the dummy input.
-                # If correct, set their score for this round to 1. Otherwise, set it to 0.
-                score = template.reward.dummy(step, resp_i)
-
-                # Update the global score of the miner.
-                # This score contributes to the miner's weight in the network.
-                # A higher weight means that the miner has been consistently responding correctly.
-                scores[i] = config.alpha * scores[i] + (1 - config.alpha) * score
-
-            bt.logging.info(f"Scores: {scores}")
-            # Periodically update the weights on the Bittensor blockchain.
-            if (step + 1) % 10 == 0:
-                # TODO(developer): Define how the validator normalizes scores before setting weights.
-                weights = torch.nn.functional.normalize(scores, p=1.0, dim=0)
-                bt.logging.info(f"Setting weights: {weights}")
-                # This is a crucial step that updates the incentive mechanism on the Bittensor blockchain.
-                # Miners with higher scores (or weights) receive a larger share of TAO rewards on this subnet.
-                result = subtensor.set_weights(
-                    netuid=config.netuid,  # Subnet to set weights on.
-                    wallet=wallet,  # Wallet to sign set weights using hotkey.
-                    uids=metagraph.uids,  # Uids of the miners to set weights for.
-                    weights=weights,  # Weights to set for the miners.
-                    wait_for_inclusion=True,
-                )
-                if result:
-                    bt.logging.success("Successfully set weights.")
-                else:
-                    bt.logging.error("Failed to set weights.")
-
-            # End the current step and prepare for the next iteration.
-            step += 1
-            # Resync our local state with the latest state from the blockchain.
-            metagraph = subtensor.metagraph(config.netuid)
-            # Sleep for a duration equivalent to the block time (i.e., time between successive blocks).
-            time.sleep(bt.__blocktime__)
-
-        # If we encounter an unexpected error, log it for debugging.
-        except RuntimeError as e:
-            bt.logging.error(e)
-            traceback.print_exc()
-
-        # If the user interrupts the program, gracefully exit.
-        except KeyboardInterrupt:
-            bt.logging.success("Keyboard interrupt detected. Exiting validator.")
             exit()
 
+        # Each validator gets a unique identity (UID) in the network for differentiation.
+        self.my_subnet_uid = self.metagraph.hotkeys.index(self.wallet.hotkey.ss58_address)
+        bt.logging.info(f"Running validator on uid: {self.my_subnet_uid}")
 
-# The main function parses the configuration and runs the validator.
+        # Step 6: Set up initial scoring weights for validation
+        bt.logging.info("Building validation weights.")
+        self.scores = torch.ones_like(self.metagraph.S, dtype=torch.float32)
+        bt.logging.info(f"Weights: {self.scores}")
+        
+        # Step 7: Set up reward functions
+        self.reward_functions = [
+            ImageRewardModel(),
+            # DiversityRewardModel()
+        ]
+        
+        # Step 8: Set up masking functions
+        self.masking_functions = [
+            BlacklistFilter(),
+            NSFWRewardModel()
+        ]
+
+        # Step 9: Set device
+        self.device = "cuda"
+
+        # Step 10: Initialise weights
+        self.weights = torch.ones_like(self.metagraph.uids , dtype = torch.float32 )
+
+        # multiply weights by the active tensor
+        self.current_block = self.subtensor.block
+        self.last_updated_block = self.subtensor.block
+
+        # loop over all last_update, any that are within 600 blocks are set to 1 others are set to 0 
+        self.weights = self.weights * self.metagraph.last_update > self.current_block - 600
+
+        # all nodes with more than 1e3 total stake are set to 0 (sets validtors weights to 0)
+        self.weights = self.weights * (self.metagraph.total_stake < 1.024e3) 
+
+        # set all nodes without ips set to 0
+        self.weights = self.weights * torch.Tensor([self.metagraph.neurons[uid].axon_info.ip != '0.0.0.0' for uid in self.metagraph.uids]) * 0.5
+
+        # move weights to device
+        self.weights = self.weights.to(self.device)
+
+        # Step 11: Initialize loop
+        self.loop = asyncio.get_event_loop()
+
+    def run(self):
+        # Step 11: The Main Validation Loop
+        bt.logging.info("Starting validator loop.")
+        step = 0
+        while True:
+            try:
+                # k = 12
+                # uids = get_random_uids(self, k=k).to(self.device)
+                device = "cuda"
+                timeout = 100
+                
+                # Call the dentrite 
+                responses = self.loop.run_until_complete( self.dendrite(self.metagraph.axons, template.protocol.Dummy(dummy_input=step), timeout = timeout))
+                
+                # Log the results for monitoring purposes.
+                bt.logging.info(f"Received response: {responses}")
+
+                # Initialise rewards tensor
+                rewards: torch.FloatTensor = torch.ones(len(responses), dtype=torch.float32).to(
+                    device
+                )
+                for masking_fn_i in self.masking_functions:
+                    mask_i, mask_i_normalized = masking_fn_i.apply(responses, )
+                    rewards *= mask_i_normalized.to(device)
+                    # TODO add wandb tracking
+                    # if not self.config.neuron.disable_log_rewards:
+                    #     event[masking_fn_i.name] = mask_i.tolist()
+                    #     event[masking_fn_i.name + "_normalized"] = mask_i_normalized.tolist()
+                    bt.logging.trace(str(masking_fn_i.name), mask_i_normalized.tolist())
+
+                for weight_i, reward_fn_i in zip([0.95], self.reward_functions):
+                    reward_i, reward_i_normalized = reward_fn_i.apply(responses)
+                    rewards += weight_i * reward_i_normalized.to(device)
+                    # TODO add wandb tracking
+                    # if not self.config.neuron.disable_log_rewards:
+                    #     event[reward_fn_i.name] = reward_i.tolist()
+                    #     event[reward_fn_i.name + "_normalized"] = reward_i_normalized.tolist()
+                    bt.logging.trace(str(reward_fn_i.name), reward_i_normalized.tolist())
+                
+                bt.logging.info(f"Rewards: {rewards}")
+
+                self.weights = self.weights + (self.config.alpha * rewards)
+
+                self.current_block = self.subtensor.block
+                if self.current_block - self.last_updated_block  >= 100:
+                    bt.logging.trace(f"Setting weights")
+
+                    # Normalize weights.
+                    self.weights = self.weights / torch.sum( self.weights )
+                    bt.logging.trace("Weights:")
+                    bt.logging.trace(self.weights)
+
+                    uids, processed_weights = bt.utils.weight_utils.process_weights_for_netuid(
+                        uids = self.metagraph.uids,
+                        weights = self.weights,
+                        netuid = self.config.netuid,
+                        subtensor = self.subtensor,
+                    )
+                    result = self.subtensor.set_weights(
+                        wallet = self.wallet,
+                        netuid = self.config.netuid,
+                        weights = processed_weights,
+                        uids = uids,
+                    )
+                    self.last_updated_block = self.current_block
+
+                    if result:
+                        bt.logging.success("Successfully set weights.")
+                    else:
+                        bt.logging.error("Failed to set weights.")
+
+                # End the current step and prepare for the next iteration.
+                step += 1
+                # Resync our local state with the latest state from the blockchain.
+                self.metagraph = self.subtensor.metagraph(self.config.netuid)
+                # Sleep for a duration equivalent to the block time (i.e., time between successive blocks).
+                time.sleep(bt.__blocktime__)
+
+            # If we encounter an unexpected error, log it for debugging.
+            except RuntimeError as e:
+                bt.logging.error(e)
+                traceback.print_exc()
+
+            # If the user interrupts the program, gracefully exit.
+            except KeyboardInterrupt:
+                bt.logging.success("Keyboard interrupt detected. Exiting validator.")
+                exit()
+
+
+def main():
+    neuron().run()
+
 if __name__ == "__main__":
-    # Parse the configuration.
-    config = get_config()
-    # Run the main function.
-    main(config)
+    main()
