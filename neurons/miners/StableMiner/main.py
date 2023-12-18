cb45ca0b2feb713d81f531757121cff0365500f2
Emperor Wang <supreme.emperor.wang@gmail.com>
2023-12-18 11:06:22 +1000

    - Fix order of imports

--- neurons/miners/StableMiner/main.py
+++ neurons/miners/StableMiner/main.py
@@ -1,11 +1,12 @@
-import pathlib, time, sys
-
-from base import StableMiner
+import time
 from neurons.shared import add_repo_to_path
 
 if __name__ == "__main__":
     ### Add the base repository to the path so the miner can access it
     add_repo_to_path()
+
+    ### Import StableMiner after fixing path
+    from base import StableMiner
 
     ### Start the miner
     with StableMiner():
--- neurons/validator/main.py
+++ neurons/validator/main.py
@@ -1,8 +1,10 @@
 from neurons.shared import add_repo_to_path
-from validator import StableValidator
 
 if __name__ == "__main__":
     ### Add the base repository to the path so the validator can access it
     add_repo_to_path()
+
+    ### Import StableValidator after fixing paths
+    from validator import StableValidator
 
     StableValidator().run()
