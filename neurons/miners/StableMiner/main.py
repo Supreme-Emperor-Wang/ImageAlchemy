f895752bed898c5eeac853c5e88ecd0101bc5d84
Emperor Wang <supreme.emperor.wang@gmail.com>
2023-12-18 11:09:37 +1000

    - Fix pathing

--- neurons/miners/StableMiner/main.py
+++ neurons/miners/StableMiner/main.py
@@ -1,9 +1,10 @@
-import time
-from neurons.shared import add_repo_to_path
+import time, pathlib, sys
 
 if __name__ == "__main__":
     ### Add the base repository to the path so the miner can access it
-    add_repo_to_path()
+    file_path = str(pathlib.Path(__file__).parent.parent.parent.parent.resolve())
+    if not file_path in sys.path:
+        sys.path.append(file_path)
 
     ### Import StableMiner after fixing path
     from base import StableMiner
--- neurons/validator/main.py
+++ neurons/validator/main.py
@@ -1,8 +1,10 @@
-from neurons.shared import add_repo_to_path
+import sys, pathlib
 
 if __name__ == "__main__":
     ### Add the base repository to the path so the validator can access it
-    add_repo_to_path()
+    file_path = str(pathlib.Path(__file__).parent.parent.parent.parent.resolve())
+    if not file_path in sys.path:
+        sys.path.append(file_path)
 
     ### Import StableValidator after fixing paths
     from validator import StableValidator
