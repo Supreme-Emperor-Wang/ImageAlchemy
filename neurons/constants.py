12dec7d662751d007efe0960247fee8a80e5f20b
Emperor Wang <supreme.emperor.wang@gmail.com>
2023-12-27 22:12:29 +1000

    - Added new constants for validator whitelists and blacklists

--- neurons/constants.py
+++ neurons/constants.py
@@ -1,3 +1,5 @@
 IA_BUCKET_NAME = "image-alchemy"
 IA_MINER_BLACKLIST = "blacklist_for_miners.json"
 IA_MINER_WHITELIST = "whitelist_for_miners.json"
+IA_VALIDATOR_BLACKLIST = "blacklist_for_validators.json"
+IA_VALIDATOR_WHITELIST = "whitelist_for_validators.json"
