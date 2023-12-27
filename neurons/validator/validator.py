5aee825d3b0fc8da4c92218c325fa8407f4c4c4f
Emperor Wang <supreme.emperor.wang@gmail.com>
2023-12-27 23:01:57 +1000

    - Specify that the background loop is for a validator

--- neurons/validator/validator.py
+++ neurons/validator/validator.py
@@ -1,34 +1,13 @@
-# The MIT License (MIT)
-# Copyright © 2023 Yuma Rao
-# TODO(developer): Set your name
-# Copyright © 2023 <your name>
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
 import argparse
 import asyncio
 import copy
 
-# Step 1: Import necessary libraries and modules
 import os
 import random
 from time import sleep
 from traceback import print_exception
 from typing import List
 
-# import this repo
 import torch
 from datasets import load_dataset
 from neurons.utils import BackgroundTimer, background_loop, get_defaults
@@ -210,7 +189,7 @@ class StableValidator:
 
         # Start the generic background loop
         self.background_steps = 1
-        self.background_timer = BackgroundTimer(300, background_loop, [self])
+        self.background_timer = BackgroundTimer(300, background_loop, [self, True])
         self.background_timer.start()
 
     def run(self):
