b4fc5ae71fea6c99be3dfc430f9f9bb3e45f35dc
Emperor Wang <supreme.emperor.wang@gmail.com>
2023-12-31 19:58:42 +1000

    - Added paths to store wandb runs

--- neurons/constants.py
+++ neurons/constants.py
@@ -1,6 +1,12 @@
+import os
+
+
 IA_BUCKET_NAME = "image-alchemy"
 IA_MINER_BLACKLIST = "blacklist_for_miners.json"
 IA_MINER_WHITELIST = "whitelist_for_miners.json"
 IA_VALIDATOR_BLACKLIST = "blacklist_for_validators.json"
 IA_VALIDATOR_WHITELIST = "whitelist_for_validators.json"
 IA_VALIDATOR_WEIGHT_FILES = "weights.json"
+
+WANDB_MINER_PATH = os.path.expanduser("~/.ImageAlchemy/wandb/miner")
+WANDB_VALIDATOR_PATH = os.path.expanduser("~/.ImageAlchemy/wandb/validator")
