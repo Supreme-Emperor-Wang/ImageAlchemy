74916ed762ec7b5dc2f395b4ebb926d565d4ecac
Emperor Wang <supreme.emperor.wang@gmail.com>
2024-01-03 13:44:43 +1000

    Reverted path change and added new constants

--- neurons/constants.py
+++ neurons/constants.py
@@ -3,19 +3,21 @@ import os
 IA_BUCKET_NAME = "image-alchemy"
 IA_MINER_BLACKLIST = "blacklist_for_miners.json"
 IA_MINER_WHITELIST = "whitelist_for_miners.json"
-IA_VALIDATOR_BLACKLIST = "blacklist_for_validators.json"
-IA_VALIDATOR_WHITELIST = "whitelist_for_validators.json"
-IA_VALIDATOR_WEIGHT_FILES = "weights.json"
 
-WANDB_MINER_PATH = os.path.expanduser("~/.ImageAlchemy/wandb/miner/wandb")
-WANDB_VALIDATOR_PATH = os.path.expanduser("~/.ImageAlchemy/wandb/validator/wandb")
+
+WANDB_MINER_PATH = os.path.expanduser("~/.ImageAlchemy/wandb/miner")
+WANDB_VALIDATOR_PATH = os.path.expanduser("~/.ImageAlchemy/wandb/validator")
 
 ### Validator only
 N_NEURONS = 10
 VPERMIT_TAO = 1024
 FOLLOWUP_TIMEOUT = 10
-
 EPOCH_LENGTH = 5
 MOVING_AVERAGE_ALPHA = 0.05
-
 EVENTS_RETENTION_SIZE = "2 GB"
+VALIDATOR_DEFAULT_REQUEST_FREQUENCY = 60
+
+IA_VALIDATOR_BLACKLIST = "blacklist_for_validators.json"
+IA_VALIDATOR_WHITELIST = "whitelist_for_validators.json"
+IA_VALIDATOR_WEIGHT_FILES = "weights.json"
+IA_VALIDATOR_SETTINGS_FILE = "validator_settings.json"
