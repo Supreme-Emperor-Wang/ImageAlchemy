c4aa914ca02a14deec604bb7d3657ed3905b049f
kmfoda <kmfoda@gmail.com>
2024-05-15 13:40:42 +0000

    Fix test_ranking failure

--- tests/e2e_test_human_reward_socres.py
+++ tests/e2e_test_human_reward_socres.py
@@ -25,6 +25,36 @@ sys.argv += [
 self = StableValidator()
 self.load_state()
 
+mock_loser_index = (
+    (
+        self.moving_average_scores
+        == torch.max(self.moving_average_scores[self.moving_average_scores > 0])
+    )
+    .nonzero()[0]
+    .item()
+)
+mock_winner_index = (
+    (
+        self.moving_average_scores
+        == torch.min(self.moving_average_scores[self.moving_average_scores > 0])
+    )
+    .nonzero()[0]
+    .item()
+)
+previous_rank = ss.rankdata(self.moving_average_scores.tolist())[mock_loser_index]
+scores = self.moving_average_scores
+for i in range(0, 10000):
+    scores = get_human_rewards(
+        self,
+        scores,
+        mock=True,
+        mock_winner=self.hotkeys[mock_winner_index],
+        mock_loser=self.hotkeys[mock_loser_index],
+    )
+    weights = torch.nn.functional.normalize(scores, p=1, dim=0)
+    print(weights[mock_loser_index].item())
+current_rank = ss.rankdata(scores.tolist())[mock_loser_index]
+
 
 def test_human_rewards_score_increase():
     test_uid_index = (
@@ -46,7 +76,7 @@ def test_human_rewards_score_increase():
     assert current_score > previous_score
 
 
-def test_human_rewards_ranking_increase():
+def test_human_weights_increase():
     test_uid_index = (
         (
             self.moving_average_scores
@@ -55,15 +85,16 @@ def test_human_rewards_ranking_increase():
         .nonzero()[0]
         .item()
     )
-    previous_rank = ss.rankdata(self.moving_average_scores.tolist())[test_uid_index]
+    weights = torch.nn.functional.normalize(self.moving_average_scores, p=1, dim=0)
+    previous_weight = weights[test_uid_index]
     scores = get_human_rewards(
         self,
         self.moving_average_scores,
         mock=True,
         mock_winner=self.hotkeys[test_uid_index],
     )
-    current_rank = ss.rankdata(scores.tolist())[test_uid_index]
-    assert current_rank > previous_rank
+    current_weight = torch.nn.functional.normalize(scores, p=1, dim=0)[test_uid_index]
+    assert current_weight > previous_weight
 
 
 def test_human_rewards_score_decrease():
@@ -101,7 +132,7 @@ def test_human_rewards_score_decrease():
     assert current_score_delta < previous_score_delta
 
 
-def test_human_rewards_ranking_decrease():
+def test_human_weights_decrease():
     mock_loser_index = (
         (
             self.moving_average_scores
@@ -118,15 +149,19 @@ def test_human_rewards_ranking_decrease():
         .nonzero()[0]
         .item()
     )
-    previous_rank = ss.rankdata(self.moving_average_scores.tolist())[mock_loser_index]
-    for i in range(0, 10000):
-        scores = self.moving_average_scores
-        scores = get_human_rewards(
-            self,
-            scores,
-            mock=True,
-            mock_winner=self.hotkeys[mock_winner_index],
-            mock_loser=self.hotkeys[mock_loser_index],
-        )
-    current_rank = ss.rankdata(scores.tolist())[mock_loser_index]
-    assert current_rank < previous_rank
+    previous_weight = torch.nn.functional.normalize(
+        self.moving_average_scores, p=1, dim=0
+    )[mock_loser_index]
+    scores = get_human_rewards(
+        self,
+        self.moving_average_scores,
+        mock=True,
+        mock_winner=self.hotkeys[mock_winner_index],
+        mock_loser=self.hotkeys[mock_loser_index],
+    )
+    current_weight = torch.nn.functional.normalize(scores, p=1, dim=0)[mock_loser_index]
+
+    assert current_weight < previous_weight
+
+
+self.background_timer.cancel()
