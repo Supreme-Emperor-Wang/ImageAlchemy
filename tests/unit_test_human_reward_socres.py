49e887136f7c7f697fbcfd2aa315fec61a3b0806
kmfoda <kmfoda@gmail.com>
2024-05-16 06:05:15 +0000

    Add test_apply_human_voting_weight

--- tests/unit_test_human_reward_socres.py
+++ tests/unit_test_human_reward_socres.py
@@ -6,12 +6,11 @@ import copy
 import time
 
 import torch
-from neurons.validator.reward import process_manual_vote
+from neurons.validator.reward import apply_human_voting_weight, process_manual_vote
 
 
-def test_manual_vote():
+def test_process_manual_vote():
     device = "cpu"
-    config = None
     start_time = time.perf_counter()
     manual_validator_timeout = 10
     reward_weights = [0.25]
@@ -43,6 +42,46 @@ def test_manual_vote():
         start_time,
         manual_validator_timeout,
         device,
+    )
+    current_reward = new_rewards[test_index].item()
+    assert current_reward > previous_reward
+
+
+def test_apply_human_voting_weight():
+    device = "cpu"
+    human_voting_weight = 0.02 / 32
+    test_index = 0
+    rewards = torch.tensor(
+        [
+            0.6522690057754517,
+            0.7715857625007629,
+            0.7447815537452698,
+            0.7694319486618042,
+            0.03637188673019409,
+            0.7205913066864014,
+            0.0890098512172699,
+            0.7766138315200806,
+            0.0,
+            0.0,
+        ]
+    ).to(device)
+    human_voting_scores = torch.tensor(
+        [
+            91 / 100,
+            1 / 100,
+            1 / 100,
+            1 / 100,
+            1 / 100,
+            1 / 100,
+            1 / 100,
+            1 / 100,
+            1 / 100,
+            1 / 100,
+        ]
+    ).to(device)
+    previous_reward = copy.copy(rewards[test_index].item())
+    new_rewards = apply_human_voting_weight(
+        rewards, human_voting_scores, human_voting_weight
     )
     current_reward = new_rewards[test_index].item()
     assert current_reward > previous_reward
