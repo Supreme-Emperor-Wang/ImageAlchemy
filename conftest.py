ba3269e88ffa91fc06a4178fd424f8d3b5cfaa94
Will Whitty <tavurth@gmail.com>
2024-05-22 11:24:41 +0800

    Fix tests top level import

--- conftest.py
+++ conftest.py
@@ -3,12 +3,12 @@
 import os
 import sys
 
-from neurons.validator import config
-
-config.IS_TEST = True
-
 # Get the absolute path of the project's root directory
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "."))
 
 # Add the project's root directory to the Python path
 sys.path.insert(0, project_root)
+
+from neurons.validator import config
+
+config.IS_TEST = True
